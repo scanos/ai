131faa277f3d14c77cd1490edd3e7f655281db95
Seamus Kane <seamuskane@aim.com>
2025-01-13 15:39:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    
+    if (name && email && message) {
+        alert('Thank you for your message!');
+        // Here you can add your code to send the form data to your server or email
+    } else {
+        alert('Please fill out all fields.');
+    }
+});
